fc17f51e573a6ac9cde999c6f660d6ea3baa6ced
mark <dev.kram5@gmail.com>
2021-05-10 21:07:50 +0800

    Initial commit

---
+++ Services/ActionCollection.cs
@@ -0,0 +1,36 @@
+﻿using CrossUtility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ObservableProperty.Services
+{
+    public class ActionCollection<TModel> where TModel : notnull
+    {
+        private readonly Dictionary<string, Func<object, Task>> actionList = new();
+
+        public ActionCollection<TModel> When<TProperty>(Expression<Func<TModel, TProperty>> expression, Action<TProperty> action)
+        {
+            var propertyName = expression.GetMemberName();
+            actionList[propertyName] = (param) =>
+            {
+                action((TProperty)param);
+                return Task.CompletedTask;
+            };
+            return this;
+        }
+
+        public ActionCollection<TModel> When<TProperty>(Expression<Func<TModel, TProperty>> expression, Func<TProperty, Task> action)
+        {
+            var propertyName = expression.GetMemberName();
+            actionList[propertyName] = (param) =>
+            {
+                return action((TProperty)param);
+            };
+            return this;
+        }
+
+        public IReadOnlyDictionary<string, Func<object, Task>> Get() => actionList;
+    }
+}
---
+++ Services/IObservablePropertyChanged.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace ObservableProperty.Services
+{
+    public interface IObservablePropertyChanged
+    {
+        ActionCollection<TModel> Observe<TModel>(TModel target) where TModel : INotifyPropertyChanged;
+    }
+}
