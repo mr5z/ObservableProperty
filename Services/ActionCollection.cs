43e562e1cd96a527ef8f464144389f22687c4116
mark <dev.kram5@gmail.com>
2021-05-13 15:03:39 +0800

    Added new method

--- Services/ActionCollection.cs
+++ Services/ActionCollection.cs
@@ -1,6 +1,7 @@
 ﻿using CrossUtility.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -10,9 +11,8 @@ namespace ObservableProperty.Services
     {
         private readonly Dictionary<string, Func<object, Task>> actionList = new();
 
-        public ActionCollection<TModel> When<TProperty>(Expression<Func<TModel, TProperty>> expression, Action<TProperty> action)
+        public ActionCollection<TModel> When<TProperty>([NotNull] string propertyName, Action<TProperty> action)
         {
-            var propertyName = expression.GetMemberName();
             actionList[propertyName] = (param) =>
             {
                 action((TProperty)param);
@@ -21,14 +21,26 @@ namespace ObservableProperty.Services
             return this;
         }
 
+        public ActionCollection<TModel> When<TProperty>([NotNull] string propertyName, Func<TProperty, Task> action)
+        {
+            actionList[propertyName] = (param) =>
+            {
+                action((TProperty)param);
+                return Task.CompletedTask;
+            };
+            return this;
+        }
+
+        public ActionCollection<TModel> When<TProperty>(Expression<Func<TModel, TProperty>> expression, Action<TProperty> action)
+        {
+            var propertyName = expression.GetMemberName();
+            return When(propertyName, action);
+        }
+
         public ActionCollection<TModel> When<TProperty>(Expression<Func<TModel, TProperty>> expression, Func<TProperty, Task> action)
         {
             var propertyName = expression.GetMemberName();
-            actionList[propertyName] = (param) =>
-            {
-                return action((TProperty)param);
-            };
-            return this;
+            return When(propertyName, action);
         }
 
         public IReadOnlyDictionary<string, Func<object, Task>> Get() => actionList;
